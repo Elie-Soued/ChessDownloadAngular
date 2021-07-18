5a2ffbd6d31a6e9e06835ee3cb9d6fcc8f47fc4c
Elie Soued <eliegebransoued@gmail.com>
2021-07-18 16:16:30 +0200

    Build a component to explore FormArray

---
+++ src/app/form-array/form-array.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-array',
+  templateUrl: './form-array.component.html',
+  styleUrls: ['./form-array.component.css'],
+})
+export class FormArrayComponent implements OnInit {
+  profileForm = this.fb.group({
+    aliases: this.fb.array([this.fb.control('')]),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {}
+
+  get aliases() {
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+    this.aliases.push(this.fb.control(''));
+  }
+}
