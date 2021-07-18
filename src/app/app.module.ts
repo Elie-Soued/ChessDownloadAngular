567b851f20ca830dd223c52301e9dab018ab0292
Elie Soued <eliegebransoued@gmail.com>
2021-07-18 17:12:29 +0200

    Delete Components that were created for training purposes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,22 +7,14 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NameEditorComponent } from './name-editor/name-editor.component';
-import { ProfileEditorComponent } from './profile-editor/profile-editor.component';
-import { FormbuilderComponent } from './formbuilder/formbuilder.component';
 import { InputComponent } from './input/input.component';
-import { FormArrayComponent } from './form-array/form-array.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NameEditorComponent,
-    ProfileEditorComponent,
-    FormbuilderComponent,
     InputComponent,
-    FormArrayComponent,
   ],
   imports: [
     BrowserModule,
