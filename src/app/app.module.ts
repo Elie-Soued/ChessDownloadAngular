c32601a5a3d708970dfc4ffaf2ea59432ee2098a
Elie Soued <eliegebransoued@gmail.com>
2021-07-23 00:18:07 +0200

    Adding Table, ResultPage, LandingPage Components and HTTPClient Module + NgbModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { InputComponent } from './input/input.component';
+import { TableComponent } from './table/table.component';
+import { ResultPageComponent } from './result-page/result-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +20,9 @@ import { InputComponent } from './input/input.component';
     HeaderComponent,
     FooterComponent,
     InputComponent,
+    TableComponent,
+    ResultPageComponent,
+    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
@@ -22,6 +30,7 @@ import { InputComponent } from './input/input.component';
     FormsModule,
     NgbModule,
     ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
