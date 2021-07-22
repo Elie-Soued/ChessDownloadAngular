c0b860d8b56abf9cfb98eacbdd3999204cc85b9f
Elie Soued <eliegebransoued@gmail.com>
2021-07-23 00:15:09 +0200

    Create the landing page and the result routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResultPageComponent } from './result-page/result-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'results', component: ResultPageComponent },
+  { path: 'landingPage', component: LandingPageComponent },
+  { path: '', redirectTo: 'landingPage', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
