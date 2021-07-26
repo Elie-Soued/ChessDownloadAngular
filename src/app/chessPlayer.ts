43ae63562eb99713325444bbf769895b1e06f97e
Elie Soued <eliegebransoued@gmail.com>
2021-07-26 11:09:42 +0200

    Create a chessPlayer interface to make a more precise api call

---
+++ src/app/chessPlayer.ts
@@ -0,0 +1,15 @@
+export interface chessPlayer {
+  avatar: string;
+  player_id: number;
+  id: string;
+  url: string;
+  name: string;
+  username: string;
+  followers: number;
+  country: string;
+  location: string;
+  last_online: number;
+  joined: number;
+  status: string;
+  is_streamer: false;
+}
