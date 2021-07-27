d6388923d41b2903b4b834609c75fed5d6807746
Elie Soued <eliegebransoued@gmail.com>
2021-07-27 16:19:40 +0200

    Correcting a mistake that took me hours : to be able to send the archives data,  the sendUserArchive had to be added to the setArchive method. (you set the Archive , then you send it )

--- src/app/input/input.component.ts
+++ src/app/input/input.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { HttpErrorResponse } from '@angular/common/http';
 import { DataService } from '../data.service';
 import { TransferService } from '../transfer.service';
 import { chessPlayer } from '../chessPlayer';
+import { archives } from '../archives';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   styleUrls: ['./input.component.css'],
 })
-export class InputComponent implements OnInit {
+export class InputComponent {
   user?: chessPlayer;
   error?: HttpErrorResponse;
-  archives = {};
+  archives?: archives;
   profileForm = this.fb.group({
     username: ['', Validators.required],
   });
@@ -24,39 +25,37 @@ export class InputComponent implements OnInit {
     private transferService: TransferService
   ) {}
 
-  ngOnInit(): void {}
-
   onSubmit() {
     this.getUser();
   }
 
-  //Setters
+  //These are the setters
 
   setUser(value: any) {
     this.user = value;
-    // console.log(value.name);
+    this.sendUserInfo(this.user);
   }
 
   setArchive(value: any) {
     this.archives = value;
-    // console.log(this.archives);
+    this.sendUserArchives(this.archives);
+    console.log(this.archives?.chess_daily.last.rating);
   }
 
   setError(value: any) {
     this.error = value;
   }
 
-  //Other Methods
+  //These are the getters
 
   getUser(): void {
     this.dataService.getPlayer(this.profileForm.value.username).subscribe(
       (values: any) => {
         this.setUser(values);
-        // console.log(values);
+        console.log(this.user);
       },
       (error) => {
         this.setError(error);
-        // console.log(error);
       }
     );
   }
@@ -65,14 +64,15 @@ export class InputComponent implements OnInit {
     this.dataService
       .getArchive(this.profileForm.value.username)
       .subscribe((values: any) => this.setArchive(values));
-    this.sendUser(this.user);
   }
 
-  sendUser(user: any) {
+  //These methods send Data to unrelated Components
+
+  sendUserInfo(user: any) {
     this.transferService.sendInfo(user);
   }
 
-  // reloadPage() {
-  //   location.reload();
-  // }
+  sendUserArchives(data: any) {
+    this.transferService.sendArchives(data);
+  }
 }
