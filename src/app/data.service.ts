9624630056970edfdad0e982ffa4c7934fea8200
Elie Soued <eliegebransoued@gmail.com>
2021-07-29 10:44:47 +0200

    Add return  types to the getArchive and the handleError methods

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { chessPlayer } from './chessPlayer';
+import { HttpErrorResponse } from '@angular/common/http';
+import { archives } from './archives';
 
 @Injectable({
   providedIn: 'root',
@@ -20,11 +22,11 @@ export class DataService {
 
   getArchive(player: string) {
     return this.http
-      .get(`https://api.chess.com/pub/player/${player}/stats`)
+      .get<archives>(`https://api.chess.com/pub/player/${player}/stats`)
       .pipe(catchError(this.handleError));
   }
 
-  handleError(err: any) {
+  handleError(err: HttpErrorResponse) {
     return throwError(err);
   }
 }
